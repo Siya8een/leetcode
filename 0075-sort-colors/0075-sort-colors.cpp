4c6e00eff9c72b1d540cded73dee1f21beba967e
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-05-19 12:15:03 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (63.29%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,35 @@
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+       int i,c1=0,c2=0,c3=0;
+       for(i=0;i<nums.size();i++)
+       {
+           switch(nums[i])
+           {
+               case 0:c1++;break;
+               case 1:c2++;break;
+               case 2:c3++;break;
+           }
+       }
+        i = 0;
+ 
+    // Store all the 0s in the beginning
+    while (c1 > 0) {
+        nums[i++] = 0;
+        c1--;
     }
+ 
+    // Then all the 1s
+    while (c2 > 0) {
+        nums[i++] = 1;
+        c2--;
+    }
+ 
+    // Finally all the 2s
+    while (c3 > 0) {
+        nums[i++] = 2;
+        c3--;
+    }
+}
 };
