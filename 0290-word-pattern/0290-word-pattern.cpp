47086572122ca138c11087ace49ec57dbe54266f
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-05-24 20:07:24 +0530

    Time: 4 ms (37.38%), Space: 6.5 MB (50.91%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
-	bool wordPattern(string pattern, string s) {
-		vector<string> v;
-		int i = 0;
-		string temp = "";
-		while(i < s.size()){
-			if(s[i] == ' '){
-				v.push_back(temp);
-				temp = "";
-			}
-			else{
-				temp += s[i];
-			}
-			i++;
-		}
-		v.push_back(temp);
-
-		if(v.size() != pattern.size()){
-			return false;
-		}
-		unordered_map<char, string> m;
-		set<string> st;
-		for(int i = 0; i < pattern.size(); i++){
-			if(m.find(pattern[i]) != m.end()){ //pattern in  hashmap
-                if(m[pattern[i]] != v[i]){
-					return false;
-				}
-			}
-			else{
-				if(st.count(v[i]) > 0){
-					return false;
-				}
-				m[pattern[i]] = v[i];
-				st.insert(v[i]);
-			}
-		}
-		return true;
-	}
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char, string> charToWord;
+        unordered_map<string, char> wordToChar;
+        vector<string> words;
+        
+        // Split the string s into words
+        istringstream iss(s);
+        string word;
+        while (iss >> word) {
+            words.push_back(word);
+        }
+        
+        // Check if the pattern and words have the same length
+        if (pattern.length() != words.size()) {
+            return false;
+        }
+        
+        // Process each character-word pair
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern[i];
+            string word = words[i];
+            
+            // Check if the character is already mapped to a different word or vice versa
+            if (charToWord.count(c) && charToWord[c] != word) {
+                return false;
+            }
+            if (wordToChar.count(word) && wordToChar[word] != c) {
+                return false;
+            }
+            
+            // Establish the bijection between the character and word
+            charToWord[c] = word;
+            wordToChar[word] = c;
+        }
+        
+        return true;
+    }
 };
