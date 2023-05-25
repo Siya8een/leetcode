71e0b35983b98486fb825138680f874e62cc5dec
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-05-25 20:09:04 +0530

    Time: 0 ms (100.00%), Space: 6 MB (68.52%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int countGoodNumbers(long long n) {
-        long long evenCount = (n + 1) / 2;  // Number of even positions
-        long long oddCount = n / 2;         // Number of odd positions
+        long long even = (n + 1) / 2;  // Number of even positions
+        long long odd = n / 2;         // Number of odd positions
 
-        // Calculate (power(5, evenCount) * power(4, oddCount)) % (10^9 + 7)
-        long long result = power(5, evenCount) % 1000000007;
-        result = (result * power(4, oddCount)) % 1000000007;
+       
+        long long result = p(5, even) % 1000000007;
+        result = (result * p(4, odd)) % 1000000007;
 
         return result;
     }
 
 private:
-    long long power(long long base, long long exp) {
+    long long p(long long base, long long exp) {
         if (exp == 0)
             return 1;
 
-        long long half = power(base, exp / 2) % 1000000007;
+        long long half = p(base, exp / 2) % 1000000007;
         long long result = (half * half) % 1000000007;
 
         if (exp % 2 == 1)
