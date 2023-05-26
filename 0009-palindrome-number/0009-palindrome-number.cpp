12a837e75b8cb02d0b71730e4891afd77aed5424
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-05-26 20:29:27 +0530

    Time: 11 ms (85.04%), Space: 5.9 MB (85.36%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,20 +1,27 @@
-#include <string>
-
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // Convert the integer to a string
-        std::string str = std::to_string(x);
+        // String s;
+        // while (x!=0){
+        //    int a=x%10;
+        //     s<< a;
+        //     x=x/10;
+        // }
+        
+        
+         string s = to_string(x);
 
-        // Check if the string is a palindrome
-        int left = 0;
-        int right = str.length() - 1;
-        while (left < right) {
-            if (str[left] != str[right])
+         int left=0;
+        int right =s.length()-1;
+       while (left < right) {
+            if (s[left] != s[right])
                 return false;
+            else
+            {
             left++;
             right--;
-        }
+             }
+       }
 
         return true;
     }
