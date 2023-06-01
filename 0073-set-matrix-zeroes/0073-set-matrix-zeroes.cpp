dd90178975a1703a15e870a788ff48750299b8e9
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-06-01 14:33:44 +0530

    Time: 27 ms (7.54%), Space: 13.4 MB (31.38%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,12 +3,13 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>> visited = matrix;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(matrix[i][j] == 0){
                     for(int k=0; k<m; k++){
-                        visited[i][k] = 0;
+                        if(matrix[i][k] != 0){
+                            matrix[i][k] = -9999;
+                        }
                     }
                 }
             }
@@ -17,14 +18,18 @@ public:
             for(int j=0; j<m; j++){
                 if(matrix[i][j] == 0){
                     for(int k=0; k<n; k++){
-                        visited[k][j] = 0;
+                        if(matrix[k][j] != 0){
+                            matrix[k][j] = 0;
+                        }
                     }
                 }
             }
         }
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                matrix[i][j] = visited[i][j];
+                if(matrix[i][j] == -9999){
+                    matrix[i][j] = 0;
+                }
             }
         }
     }
