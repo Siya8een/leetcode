0cd3f318eb54d267b6d807e67e332509b01cf572
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-06-02 13:22:41 +0530

    Time: 153 ms (29.19%), Space: 93.2 MB (80.74%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,38 +1,19 @@
-// class Solution {
-// public:
-//     int maxProfit(vector<int>& prices) {
-//         int n = prices.size();
-//         int m = INT_MIN;
-//         int l = INT_MAX;
-        
-//         for (int i = n - 1; i >= 0; i--) {
-//             m = max(m, prices[i]);
-//         }
-        
-//         if (prices[0] > l) {
-//             for (int i = 0; i < n; i++) {
-//                 l = min(l, prices[i]);
-//             }
-            
-//             return m - l;
-//         } else {
-//             return 0;
-//         }
-//     }
-// };
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        int minPrice = INT_MAX;
-        int maxProfit = 0;
+        int lsf = INT_MAX;
+        int op = 0;
+        int pist = 0;
         
-        for (int i = 0; i < n; i++) {
-            minPrice = min(minPrice, prices[i]);
-            maxProfit = max(maxProfit, prices[i] - minPrice);
+        for(int i = 0; i < prices.size(); i++){
+            if(prices[i] < lsf){
+                lsf = prices[i];
+            }
+            pist = prices[i] - lsf;
+            if(op < pist){
+                op = pist;
+            }
         }
-        
-        return maxProfit;
+        return op;
     }
 };
-
