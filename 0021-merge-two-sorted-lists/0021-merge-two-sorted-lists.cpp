ef8bf6ac16affddf88ef07d45f3842865da51e5f
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-06-03 13:57:10 +0530

    Time: 7 ms (73.22%), Space: 14.7 MB (97.93%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* dummy = new ListNode(-1);  
-        ListNode* current = dummy;  
-        
-        while (list1 != NULL && list2 != NULL) {
-            if (list1->val <= list2->val) {  
-                current->next = list1;
-                list1 = list1->next;
-            } else {
-                current->next = list2;
-                list2 = list2->next;
-            }
-            current = current->next; 
-        }
-        
-        if (list1 != NULL) {
-            current->next = list1;  
-        }
-        if (list2 != NULL) {
-            current->next = list2;  
-        }
-        
-        return dummy->next;  
-    }
-};
+	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
+  {
+		// if list1 happen to be NULL
+		// we will simply return list2.
+		if(l1 == NULL)
+        {
+			return l2;
+		}
+		
+		// if list2 happen to be NULL
+		// we will simply return list1.
+		if(l2 == NULL)
+        {
+			return l1;
+		} 
+		
+		// if value pointend by l1 pointer is less than equal to value pointed by l2 pointer
+		// we wall call recursively l1 -> next and whole l2 list.
+		if(l1 -> val <= l2 -> val)
+        {
+			l1 -> next = mergeTwoLists(l1 -> next, l2);
+			return l1;
+		}
+		// we will call recursive l1 whole list and l2 -> next
+		else
+        {
+			l2 -> next = mergeTwoLists(l1, l2 -> next);
+			return l2;            
+		}
+	}
+};	
