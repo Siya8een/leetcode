6e39fd2e34dc27730126fb5db50e57572d60fbf9
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-06-09 13:56:11 +0530

    Time: 4 ms (26.48%), Space: 6.3 MB (63.04%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int count(int i, int j, vector<vector<int>>& dp, int m, int n) {
+        if (i >= n || j >= m) {
+            return 0;
+        } else if (i == n - 1 && j == m - 1) {
+            return 1;
+        } else if (dp[i][j] != -1) {
+            return dp[i][j];
+        } else {
+            return dp[i][j] = count(i + 1, j, dp, m, n) + count(i, j + 1, dp, m, n);
+        }
+    }
+
+    int uniquePaths(int m, int n) {
+        int i = 0;
+        int j = 0;
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return count(i, j, dp, m, n);
+    }
+};
