7360091cffbf1198b793fdfe465ef31f78754a49
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-06-13 10:24:47 +0530

    Time: 4 ms (22.02%), Space: 5.9 MB (52.83%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -3,11 +3,18 @@ public:
     int reverse(int x) {
         long reverse = 0;
         while(x!=0){
-            int digit = x%10;
-            reverse = reverse*10 + digit;
+            int a = x%10;
+            reverse = reverse*10 + a;
             x=x/10;
         }
-       if(reverse>INT_MAX || reverse<INT_MIN) return 0;
+        //to check in range 
+       if(reverse>INT_MAX || reverse<INT_MIN) {
+           return 0;
+       }
+        
+    
         return reverse;
+        
+        
     }
 };
