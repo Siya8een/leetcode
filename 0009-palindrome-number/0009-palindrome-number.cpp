58244d268b59c207e93b394bf6e43e07dbc19a35
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-06-14 01:36:13 +0530

    Time: 44 ms (5.12%), Space: 5.8 MB (85.34%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,28 +1,50 @@
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+//         string s;
+//         while (x!=0){
+//            int a=x%10;
+//             s
+//             x=x/10;
+//         }
+        
+        
+//          // string s = to_string(x);
+
+//          int left=0;
+//         int right =s.length()-1;
+//        while (left < right) {
+//             if (s[left] != s[right])
+//                 return false;
+//             else
+//             {
+//             left++;
+//             right--;
+//              }
+//        }
+
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // String s;
-        // while (x!=0){
-        //    int a=x%10;
-        //     s<< a;
-        //     x=x/10;
-        // }
-        
-        
-         string s = to_string(x);
-
-         int left=0;
-        int right =s.length()-1;
-       while (left < right) {
-            if (s[left] != s[right])
-                return false;
-            else
-            {
-            left++;
-            right--;
-             }
-       }
-
-        return true;
+        int num = x;
+         long y = 0;
+        if(x<0){
+            return false;
+        }
+        while(num>0)
+        {
+           int rem=num%10;
+            num=num/10;
+            y=y*10+rem;
+        }
+        cout<<y<<endl;
+        if(y==x){
+            return true;
+            }
+         return false;
     }
 };
