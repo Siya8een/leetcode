fdaf1e4f4dc5798cb93483d59b53aab810f39096
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-06-14 14:07:43 +0530

    Time: 4 ms (39.90%), Space: 5.8 MB (79.69%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.cpp
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
-       bool isThree(int n) {
-        int count=0;
-        for(int i=1;i<=n;i++){
-           if(n%i==0) count++; 
+    bool isThree(int n) {
+        if (n < 4)
+            return false;
+
+        int sqrtN = sqrt(n);
+
+        // Check if n is a perfect square
+        if (sqrtN * sqrtN != n)
+            return false;
+
+        // Check if the square root of n is a prime number
+        for (int i = 2; i * i <= sqrtN; i++) {
+            if (sqrtN % i == 0)
+                return false;
         }
-        if(count==3) return true;
-        else return false;
+
+        return true;
     }
 };
