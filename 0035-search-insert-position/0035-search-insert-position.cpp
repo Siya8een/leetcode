5513a5bb9a19baaefe6ce433c45d903b11dd4450
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-06-18 16:19:59 +0530

    Time: 5 ms (61.84%), Space: 9.6 MB (56.74%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,22 @@
+     class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1; // subtract 1 to avoid accessing out-of-bounds element
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2; // use this formula to avoid integer overflow
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        // target not found, return the index where it would be inserted
+        return left;
+    }
+};
+ 
