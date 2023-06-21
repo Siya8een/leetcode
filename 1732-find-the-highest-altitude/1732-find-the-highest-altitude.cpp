cb357dc27372c0d9174c598224e2bebc304e1f51
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-06-21 22:18:55 +0530

    Time: 6 ms (31.61%), Space: 8 MB (51.00%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -5,12 +5,9 @@ public:
         int sum1=0;
         for (int i=0;i<gain.size();i++){
             sum1 = sum1 + gain[i];
-            //sum = sum1;
-           // if ( sum>0){
+            
                 sum = max ( sum, sum1);
-               // sum1 = sum;
-           // }
-            //sum1 = sum;
+               
         }
         return sum;
     }
