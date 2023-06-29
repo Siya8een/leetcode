78fb1603e0f1c3f056d432101cf657b1f2f2906f
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-06-30 01:38:12 +0530

    Time: 5 ms (84.94%), Space: 6.4 MB (93.34%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        if ( n==1){
+        if (n==1){
             return "1";
         }
-          if ( n==2){
+        if (n==2){
             return "11";
         }
-         string s = countAndSay(n - 1);
-        int cnt = 1;
-        string ans = "";
-        for (int i = 0; i < s.size(); i++) {
-            if (i < s.size() && s[i] == s[i + 1]) {
-                cnt++;
-            } else {
-               ans += char(cnt + '0');
-                ans += s[i]; 
-                cnt = 1;
+        
+        string s = countAndSay(n-1);
+        string ans;
+        int count =1;
+        for ( int i=0; i < s.size();i++){
+            
+            if ( s[i]==s[i+1] && i < s.size()){
+                count++;
             }
+            else {
+           ans += ( count +  '0');
+             ans += s[i]; 
+            count =1;
+            }
+            
         }
         return ans;
     }
