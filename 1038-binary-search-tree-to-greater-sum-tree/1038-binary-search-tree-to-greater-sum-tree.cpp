004bc8ebf2df518bdd911c291786232249245406
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-07-02 18:33:42 +0530

    Time: 6 ms (29.80%), Space: 8 MB (81.42%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -9,26 +9,41 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     void sum(TreeNode* root) {
+//         int sum =0;
+//         if ( root == NULL){
+//             return ;
+//         }
+//          sum( root->right);
+//         sum = sum + root -> val;
+//          root -> val = sum;
+//         sum ( root-> left);
+
+//     }
+//     TreeNode* bstToGst(TreeNode* root) {
+//         sum( root);
+//         return root ;
+//     }
+// };
 class Solution {
 public:
-    int sum = 0;
-    
-    void inorderTraversal(TreeNode* root) {
-        if (root == nullptr) {
+    int summ = 0;
+    void sum(TreeNode* root) {
+        
+        if (root == NULL) {
             return;
         }
-        
-        inorderTraversal(root->right); // Traverse right subtree first
-        
-        sum += root->val; // Update the sum
-        
-        root->val = sum; // Update the node's value with the sum
-        
-        inorderTraversal(root->left); // Traverse left subtree
+        sum(root->right);
+        summ = summ + root->val;
+        root->val = summ;
+        sum(root->left);
     }
-    
+
     TreeNode* bstToGst(TreeNode* root) {
-        inorderTraversal(root);
+        sum(root);
         return root;
     }
 };
+
