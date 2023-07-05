fda36ec866d05da2663df25de79a028a3422d03d
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-07-05 20:40:35 +0530

    Time: 28 ms (59.86%), Space: 13.6 MB (86.84%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.cpp
+++ 0929-unique-email-addresses/0929-unique-email-addresses.cpp
@@ -1,35 +1,17 @@
 class Solution {
 public:
     int numUniqueEmails(vector<string>& emails) {
-        unordered_set<string> uniqEmails;
-        for(string& email: emails) {
-            bool inDomain = false;
-            string actEmail;
-            int i = 0;
-            for(int i = 0; i < email.size(); ) {
-                char c = email[i];
-                if (inDomain) {
-                    actEmail.push_back(c);
-                    i++;
-                } else {
-                    if (c == '@') {
-                        actEmail.push_back(c);
-                        inDomain = true;
-                        i++;
-                    } else if (c == '.') {
-                        i++;
-                    } else if (c == '+') {
-                        while(email[i] != '@') {
-                            i++;
-                        }
-                    } else {
-                        actEmail.push_back(c);
-                        i++;
-                    }
-                }
-            }
-            uniqEmails.insert(actEmail);
+    unordered_set<string> st;
+    for(string &email : emails) {
+        string cleanEmail;
+        for(char c : email) {
+            if(c == '+' || c == '@') break;
+            if(c == '.') continue;
+            cleanEmail += c;
         }
-        return uniqEmails.size();
+        cleanEmail += email.substr(email.find('@'));
+        st.insert(cleanEmail);
     }
+    return st.size();
+}
 };
