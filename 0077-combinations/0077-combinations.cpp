c645c008434192e854fda5296145ff23211e0de6
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-07-07 20:33:56 +0530

    Time: 552 ms (8.77%), Space: 164 MB (15.13%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+   
+  void solve(int n, int k, vector <vector <int>>& ans, vector <int> output,int index){
+        if ( output.size()>= k){
+             ans .push_back( output);
+            return ;
+            
+        }
+        
+        for ( int i=index; i<= n;i++)
+        {
+           
+            output.push_back( i );
+             solve ( n,k, ans,output, i+1);
+            output.pop_back();
+        }
+ 
+    }
+    
+    vector<vector<int>> combine(int n, int k) {
+         vector<vector<int>> ans;
+        vector<int> output;
+        solve(n, k, ans, output, 1);
+        return ans;
+    }
+};
