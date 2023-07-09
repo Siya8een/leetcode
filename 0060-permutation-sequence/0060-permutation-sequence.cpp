e03a4055cc384e11530d56699241b9808623b038
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-07-09 18:57:40 +0530

    Time: 189 ms (14.77%), Space: 5.9 MB (83.28%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -5,13 +5,11 @@ public:
         for (int i = 1; i <= n; i++) {
             nums.push_back('0' + i);
         }
-        
-        string ans = nums;
         for (int i = 1; i < k; i++) {
             next_permutation(nums.begin(), nums.end());
-            ans = nums;
+          
         }
         
-        return ans;
+        return nums;
     }
 };
