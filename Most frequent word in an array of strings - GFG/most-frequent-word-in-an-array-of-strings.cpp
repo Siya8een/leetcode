1ae168e1b1790a71f2201212996d53622a87ff14
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-07-12 12:38:26 +0530

    Added solution - LeetHub

--- Most frequent word in an array of strings - GFG/most-frequent-word-in-an-array-of-strings.cpp
+++ Most frequent word in an array of strings - GFG/most-frequent-word-in-an-array-of-strings.cpp
@@ -14,37 +14,32 @@ class Solution
     string mostFrequentWord(string arr[], int n) 
     {
         // code here
-         unordered_map<string , int> freq;
-         unordered_map< string ,int > occurrence;
-         
-         int k=1;
-         for ( int i=0; i< n;i++){
-             if ( occurrence.count( arr[i]) >0){
-                 continue;
+         unordered_map <string, int > freq;
+         unordered_map < string , int > occurrence;
+         int k =1;
+         for ( int i=0; i< n ; i++){
+             if ( occurrence.count(arr[i])>0){
+                continue; 
              }
-            
-                     occurrence[arr[i]] = k++;
-           
+             occurrence [arr[i]]= k++;
          }
          int maxx = INT_MIN;
-         string result ;
-         for ( int i=0; i<n;i++){
-              freq[arr[i]]++;
-              if (maxx <= freq[arr[i]]) {
-                 if ( maxx < freq[arr[i]]){
-                      maxx = freq[arr[i]];
-                       result = arr[i];
+         string result = "";
+         for ( int i=0;i<n;i++){
+             freq[arr[i]]++;
+             if ( maxx <= freq[arr[i]]){
+                 if (maxx < freq[arr[i]]){
+                     maxx = freq[arr[i]];
+                     result = arr[i];
                  }
-                  else {
-                      if (occurrence[result]
+                 if (occurrence[result]
                         < occurrence[arr[i]]) {
                         maxx= freq[arr[i]];
-                        result = arr[i];
-                    }
-                  }
+                        result = arr[i]; 
+                 }
              }
          }
-          return result;
+         return result;
     }
 };
 
