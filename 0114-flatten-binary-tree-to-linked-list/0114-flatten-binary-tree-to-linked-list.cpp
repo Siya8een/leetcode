ba1116dd04ebd3699f3e2a76ec7d71bf74015d58
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-08-02 09:47:23 +0530

    Time: 6 ms (61.78%), Space: 12.7 MB (52.25%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -13,22 +13,27 @@ class Solution {
 public:
     void flatten(TreeNode* root) {
         if(root==NULL)
-        return;
+            return;
+        // Store the original right child in a temporary variable
+        TreeNode* temp=root->right;
+        
+        // Make the left subtree the new right subtree
+        root->right=root->left;
+        root->left=NULL;
 
-        if(root->left!=NULL){
-
-            flatten(root->left);
-
-            TreeNode* temp=root->right;
-            root->right=root->left;   /*KEY LINE*/
-            root->left=NULL;
-
-            TreeNode* t=root->right;
-            while(t->right!=NULL){
-                t=t->right;
-            }
-            t->right=temp;
+        // Find the rightmost node of the newly attached left subtree
+        TreeNode* rightMost=root;
+        while(rightMost->right)
+        {
+            rightMost=rightMost->right;
         }
-        flatten(root->right);
+
+        // Attach the original right subtree to the rightmost node of the new right subtree
+        rightMost->right=temp;
+
+        // Update the root to the current right child and continue the recursive call
+        root=root->right;
+        flatten(root);
+
     }
 };
