f025349c28fcce9cc990ecdd9d541fce0e7f3a8a
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-08-15 06:42:35 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (94.28%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    int rob(std::vector<int>& nums) {
-        int n = nums.size();
-        
-        if (n == 0) {
-            return 0;
+   
+    int bottomUp(vector<int>& nums,int n){
+       
+        int prev2=0;
+        int prev1=nums[0];
+        int curr=0;
+        for(int i=1;i<=n;i++){
+            int temp=0; 
+            if(i-2>=0)
+                temp=prev2;
+         int include=nums[i]+temp;
+         int exclude=prev1;
+            curr=max(include,exclude);
+            prev2=prev1;
+            prev1=curr;
         }
-        if (n == 1) {
-            return nums[0];
-        }
+        return prev1;
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size()-1;
+        return bottomUp(nums,n);
         
-        std::vector<int> dp(n, 0);
-        
-        dp[0] = nums[0];
-        dp[1] = std::max(nums[0], nums[1]);
-        
-        for (int i = 2; i < n; i++) {
-            dp[i] = std::max(dp[i - 1], dp[i - 2] + nums[i]);
-        }
-        
-        return dp[n - 1];
     }
 };
