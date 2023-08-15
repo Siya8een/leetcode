166b8864e44aa7a50a076b042dc3f267b7e7fd91
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-08-15 15:48:04 +0530

    Time: 3 ms (94.76%), Space: 13.8 MB (18.84%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-     int prod1=nums[0],prod2=nums[0],result=nums[0];
-        int n=nums.size();
-        for(int i=1;i<n;i++)
-        {
-            int temp= max({nums[i],prod1*nums[i],prod2*nums[i]});
-            prod2=min({nums[i],prod1*nums[i],prod2*nums[i]});
-            prod1=temp;
-
-            result= max(result,prod1);
+        if (nums.empty()) {
+            return 0; // Return 0 for an empty array
         }
-        return result;
+        
+        int currentMaxPositive = nums[0]; // Maximum positive product ending at index i
+        int currentMinNegative = nums[0]; // Minimum negative product ending at index i
+        int globalMax = nums[0];          // Maximum product seen so far
+        
+        for (int i = 1; i < nums.size(); i++) {
+            int num = nums[i];
+            
+            if (num < 0) {
+                // Update currentMaxPositive and currentMinNegative based on the current number
+                int temp = currentMaxPositive;
+                currentMaxPositive = max(num, currentMinNegative * num);
+              currentMinNegative = min(num, temp * num);
+            } else {
+                // Update currentMaxPositive and currentMinNegative for positive numbers
+                currentMaxPositive = max(num, currentMaxPositive * num);
+              currentMinNegative = min(num, currentMinNegative * num);
+            }
+            
+            // Update the global maximum product if necessary
+            globalMax = max(globalMax, currentMaxPositive);
+        }
+        
+        return globalMax; // Return the maximum product
     }
 };
