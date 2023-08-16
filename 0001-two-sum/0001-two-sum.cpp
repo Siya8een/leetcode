3a5c8981f56d3dc0c498402304a2422e80c51c28
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-08-16 22:08:04 +0530

    Time: 5 ms (96.09%), Space: 10.5 MB (51.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,55 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
-       
-        for(int i = 0; i < nums.size(); i++){
-            if(mp.find(target - nums[i]) == mp.end())
-                mp[nums[i]] = i;
-            else
-                return {mp[target - nums[i]], i};
+    int index(int element, vector<int>& nums) {
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] == element) {
+                return i;
+            }
         }
- 
-        return {-1, -1};
+        return -1;
+    }
+
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> ans;
+
+        vector<int> original = nums;
+        sort(original.begin(), original.end());
+
+        int left = 0;
+        int right = n - 1;
+
+        while (left < right) {
+            int sum = original[left] + original[right];
+
+            if (sum == target) {
+                int idx1 = index(original[left], nums);
+                int idx2 = index(original[right], nums);
+                
+                // Avoid using the same index twice
+                if (idx1 == idx2) {
+                    idx2 = index(original[right], nums, idx1);
+                }
+
+                ans.push_back(idx1);
+                ans.push_back(idx2);
+                return ans;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return ans;
+    }
+    
+    int index(int element, vector<int>& nums, int skipIndex) {
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] == element && i != skipIndex) {
+                return i;
+            }
+        }
+        return -1;
     }
 };
