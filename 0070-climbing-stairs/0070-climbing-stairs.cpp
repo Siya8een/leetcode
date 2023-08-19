2a60839a291e72b8297fee251371f13610e719c4
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-08-19 17:37:59 +0530

    Time: 2 ms (40.59%), Space: 6.4 MB (15.86%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     int climbStairs(int n) {
-         vector <int> memo(n+1, -1);
-         return climb(n, memo);
-    }
-    
-    int climb(int n, vector<int>& memo) {
-        if (n <= 2) {
-            return n;
+        vector <int > dp ( n+1);
+         if ( n==1)
+         {
+             return 1;
+         } 
+        if ( n==2)
+         {
+             return 2;
+         } int prev1=1;
+       int  prev2=2;
+        int x;
+        for ( int i= 3;i <= n ;i++){
+      x   = prev1+ prev2;
+           prev1=prev2;
+            prev2 = x;
+            
+            
         }
-        
-        if (memo[n] != -1) {
-            return memo[n];
-        }
-        
-        memo[n] = climb(n-1, memo) + climb(n-2, memo);
-        return memo[n];
+         return x;
     }
 };
