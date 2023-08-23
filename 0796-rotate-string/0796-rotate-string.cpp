cea8bd04adc4ee9df1b8c62b3705cfe99d803117
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-08-24 00:54:03 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (89.49%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -5,8 +5,8 @@ public:
             return false; // If the lengths are different, rotation is not possible
         }
         
-        string doubleS = s + s; // Concatenate s with itself
+        string d = s + s; // Concatenate s with itself
         
-        return doubleS.find(goal) != string::npos; // Check if goal is a substring of doubleS
+        return d.find(goal) != string::npos; 
     }
 };
