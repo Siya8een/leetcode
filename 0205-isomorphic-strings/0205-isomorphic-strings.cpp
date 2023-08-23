572b01c017c72a03a8a64f42a19e01a050889f9f
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-08-24 00:06:45 +0530

    Time: 8 ms (47.82%), Space: 7.1 MB (17.44%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,32 +1,28 @@
-
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int n = s.length();
-        
-        if (n != t.length()) {
-            return false; // If the lengths of the strings are not equal, they cannot be isomorphic.
-        }
-        
-        unordered_map<char, char> sToT; // Map to store the mapping from characters in s to characters in t
-        unordered_map<char, char> tToS; // Map to store the mapping from characters in t to characters in s
-        
-        for (int i = 0; i < n; i++) {
-            char charS = s[i];
-            char charT = t[i];
-            
-            if (sToT.find(charS) == sToT.end() && tToS.find(charT) == tToS.end()) {
-                // If both characters are not in the maps, add them to the maps.
-                sToT[charS] = charT;
-                tToS[charT] = charS;
-            } else {
-                // If either of the characters is already in the map, check if the mapping is valid.
-                if (sToT[charS] != charT || tToS[charT] != charS) {
-                    return false;
+        int n = s.size();
+        if(s.size()!= t.size()) return false;
+        map<char, char> mpp;
+
+        for(int i=0; i<n; i++)
+        {
+            if(mpp.find(s[i]) != mpp.end())
+            {
+                if(mpp[s[i]]==t[i]) continue;
+                else return false;
+            }
+            // else mpp[s[i]] = t[i];
+            else
+            {
+                for(auto it:mpp)
+                {
+                    if(it.second == t[i]) return false;
+                    else continue;
                 }
+                mpp[s[i]] = t[i];
             }
         }
-        
-        return true; // If all characters are correctly mapped, the strings are isomorphic.
+        return true;       
     }
 };
