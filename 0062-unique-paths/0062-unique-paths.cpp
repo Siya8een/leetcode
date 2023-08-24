3b484c00b89e6b6e3d3dfcc7e24b43d2814debbb
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-08-25 00:39:47 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (22.68%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    int solve(int m, int n, vector<vector<int>>& dp) {
-        if (m == 0 && n == 0) {
-            return 1;
-        }
-        if (m < 0 || n < 0) {
-            return 0;
-        }
-        if (dp[m][n] != -1) {
-            return dp[m][n];
-        }
-
-        dp[m][n] = solve(m - 1, n, dp) + solve(m, n - 1, dp);
-        return dp[m][n];
-    }
-
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1)); // Initialize dp table with -1
-        return solve(m - 1, n - 1, dp); // Start solving from bottom-right corner
+        vector<vector<int>> dp(m, vector<int>(n, 0)); // Initialize dp table with 0
+        dp[0][0] = 1;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i > 0) {
+                    dp[i][j] += dp[i - 1][j]; // Move from above
+                }
+                if (j > 0) {
+                    dp[i][j] += dp[i][j - 1]; // Move from the left
+                }
+            }
+        }
+        
+        return dp[m - 1][n - 1]; // Return the result at the bottom-right corner
     }
 };
