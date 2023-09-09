dc7fa9fdb4c5566a04bef463b75ebc1ca2d8c26e
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-09-09 20:14:59 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (21.11%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -17,17 +17,3 @@ public:
     }
 };
 
-
-// In the corrected code, the line `vector<int> row(i + 1, 1);` initializes a vector named `row` with `i + 1` elements, all initialized to the value `1`.
-
-// Let's break down the initialization:
-
-// - `vector<int>` declares a vector of integers.
-// - `row` is the name of the vector variable we are declaring.
-// - `(i + 1, 1)` is the initializer part of the declaration.
-// - `i + 1` specifies the size of the vector, which is equal to the row number (`i`) plus one. Since row numbers start from 0, we add 1 to get the correct size.
-// - `, 1` initializes all the elements of the vector to the value `1`.
-
-// For example, if `i` is 3, then `row` will be initialized as `{1, 1, 1, 1}` because the size is `i + 1` which is `3 + 1 = 4`, and all the elements are set to `1` as specified by the initializer.
-
-// So, this initialization ensures that the `row` vector has the correct size for each row of Pascal's triangle, and all elements in the row are initially set to `1`.
