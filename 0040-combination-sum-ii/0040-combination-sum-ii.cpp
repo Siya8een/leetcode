115b03ea542672affe7a85ff469ab22b8af32cc5
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-09-10 11:51:58 +0530

    Time: 3 ms (85.01%), Space: 10.6 MB (78.46%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
-    void solve(vector<vector<int>>& ans, vector<int>& candidates, vector<int>& r, int target, int index) {
+    
+    void solve(vector<vector<int>>& ans, vector<int>& ds, int index, vector<int>& candidates, int target) {
         if (target == 0) {
-            ans.push_back(r);
+             
+            ans.push_back(ds);
+            
             return;
         }
-
-        for (int i = index; i < candidates.size(); ++i) {
+        
+        for (int i = index; i < candidates.size(); i++) {
             // Skip duplicates to avoid duplicate combinations
-            if (i > index && candidates[i] == candidates[i - 1])
+            if (i > index && candidates[i] == candidates[i - 1]) {
                 continue;
-
-            if (target < candidates[i])
-                return;  // Optimization: candidates are sorted, so further candidates won't work
-
-            r.push_back(candidates[i]);
-            solve(ans, candidates, r, target - candidates[i], i + 1);
-            r.pop_back();
+            }
+            
+            if (candidates[i] > target) {
+                break; // If the candidate is larger than the target, stop considering it
+            }
+            
+            ds.push_back(candidates[i]);
+            solve(ans, ds, i + 1, candidates, target - candidates[i]);
+          
+            ds.pop_back();
         }
     }
-
+    
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
-        int index = 0;
+        vector<int> ds;
         vector<vector<int>> ans;
-        vector<int> r;
-        solve(ans, candidates, r, target, index);
+        sort(candidates.begin(), candidates.end()); // Sort candidates to handle duplicates properly
+        int index = 0;
+        solve(ans, ds, index, candidates, target);
         return ans;
     }
 };
