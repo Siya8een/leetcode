449494d7e99cac6f90a80c03bb02894d1c95ef2d
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-09-11 23:58:01 +0530

    Time: 10 ms (32.85%), Space: 14 MB (52.56%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+    void dfs(int sr, int sc, vector<vector<int>>& image, int color, vector<vector<int>>& ans, int initialcolor, vector<int>& drow, vector<int>& dcol) {
+        ans[sr][sc] = color;  // Update the current cell with the new color
         int n = image.size();
         int m = image[0].size();
-        int oldColor = image[sr][sc]; // Store the original color
         
-        if (oldColor == newColor) {
-            return image; // No need to perform flood fill if the new color is the same as the old color
-        }
-        
-        queue<pair<int, int>> q;
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        
-        q.push({sr, sc});
-        vis[sr][sc] = 1; // Mark the starting pixel as visited
-        
-        int drow[] = {-1, 0, 1, 0};
-        int dcol[] = {0, -1, 0, 1};
-        
-        while (!q.empty()) {
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
-            
-            image[r][c] = newColor; // Change the color of the current pixel
-            
-            for (int i = 0; i < 4; i++) {
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
-                
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0 && image[nrow][ncol] == oldColor) {
-                    q.push({nrow, ncol});
-                    vis[nrow][ncol] = 1; // Mark the neighboring pixel as visited
-                }
+        for (int i = 0; i < 4; i++) {
+       
+            int nrow = sr + drow[i];
+            int ncol = sc + dcol[i];
+            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == initialcolor && ans[nrow][ncol] != color) {
+                dfs(nrow, ncol, image, color, ans, initialcolor, drow, dcol); 
             }
         }
+    }
+    
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int initialcolor = image[sr][sc];
+        vector<vector<int>> ans = image;
+        vector<int> drow = {-1, 0, 1, 0};
+        vector<int> dcol = {0, 1, 0, -1};
         
-        return image;
+        dfs(sr, sc, image, color, ans, initialcolor, drow, dcol);
+        
+        return ans;
     }
 };
