d8e32c5e7cd6f6cd7a5202d239b8181c5815a52b
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-10-25 14:51:25 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (45.43%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divedabit-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1716-calculate-money-in-leetcode-bank/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divedabit-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1716-calculate-money-in-leetcode-bank.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divedabit-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1716-calculate-money-in-leetcode-bank/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divedabit-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1716-calculate-money-in-leetcode-bank.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int total = 0;
-        int weeks = n / 7;  // Calculate the number of full weeks
-        int days_in_last_week = n % 7;  // Calculate the number of days in the last week
 
-        // Calculate the total contribution for full weeks
-        total += 28 * weeks + 7 * (weeks * (weeks - 1) / 2);
+        int complete_weeks = n / 7 , inc_weeks = n % 7 ;
 
-        // Calculate the contribution for the last week
-        total += (days_in_last_week * (days_in_last_week + 1)) / 2 + weeks * days_in_last_week;
+        // 28 + 35 + 42 + .. xterms 
 
-        return total;
+        int ans = ((complete_weeks) * ( 2 * 28 + ( complete_weeks - 1 ) * 7 ))/2;
+
+        // complete weeks + 1 + ... + (complete_weeks + inc_weeks) [ terms ->  inc_weeks ] 
+
+        ans +=  ((inc_weeks) * ( 2 * (complete_weeks + 1) + ( inc_weeks - 1 ) * 1 ))/2;
+
+        return ans;
+
     }
 };
