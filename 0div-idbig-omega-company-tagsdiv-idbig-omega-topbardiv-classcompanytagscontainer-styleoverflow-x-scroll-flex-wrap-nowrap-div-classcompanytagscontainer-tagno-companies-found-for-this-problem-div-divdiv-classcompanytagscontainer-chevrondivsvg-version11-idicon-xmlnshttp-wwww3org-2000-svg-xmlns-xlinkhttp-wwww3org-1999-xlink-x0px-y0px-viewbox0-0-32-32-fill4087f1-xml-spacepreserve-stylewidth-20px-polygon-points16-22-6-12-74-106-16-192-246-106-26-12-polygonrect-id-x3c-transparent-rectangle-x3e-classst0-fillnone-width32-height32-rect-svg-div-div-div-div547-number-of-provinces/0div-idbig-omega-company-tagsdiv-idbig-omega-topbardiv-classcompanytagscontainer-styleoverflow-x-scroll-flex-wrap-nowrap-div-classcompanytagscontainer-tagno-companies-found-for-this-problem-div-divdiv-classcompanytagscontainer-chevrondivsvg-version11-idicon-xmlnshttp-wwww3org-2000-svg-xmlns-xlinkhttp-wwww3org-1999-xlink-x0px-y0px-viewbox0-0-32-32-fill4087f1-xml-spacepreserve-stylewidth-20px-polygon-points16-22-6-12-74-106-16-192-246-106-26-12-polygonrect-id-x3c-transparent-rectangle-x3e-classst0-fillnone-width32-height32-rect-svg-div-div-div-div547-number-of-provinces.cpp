68fd7403aaa8d0c84fcc391c6d1f66afab3a04cf
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2023-12-26 03:10:43 +0530

    Time: 13 ms (98.49%), Space: 17.1 MB (32.54%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div547-number-of-provinces/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div547-number-of-provinces.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div547-number-of-provinces/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div547-number-of-provinces.cpp
@@ -1,39 +1,57 @@
 class Solution {
 public:
     
-   void dfs(int i, vector<int>& vis, vector<vector<int>>& adjList) {
-        vis[i] = 1;
-        for (int neighbor : adjList[i]) {
-            if (vis[neighbor] == 0) {
-                dfs(neighbor, vis, adjList);
-            }
-        }
-    }
+    void dfs(vector<int> adjlist[],   vector <int >& vis,int count, int i){
+          vis [i]=1;
+         
+          for ( auto it : adjlist[i]){
+              if ( vis[it]==0)
+              dfs (adjlist , vis,count, it);
+          }
+         
+      }
+    
+    
     
     int findCircleNum(vector<vector<int>>& isConnected) {
+        
         int n = isConnected.size();
-    // creating an. adjacency list 
-         vector < int > vis ( n, 0);
-        vector < vector < int >> adjlis(n);
-         for ( int i =0 ;i < isConnected.size();i++){
-             for ( int j =0 ; j < isConnected[0].size();j++){
-                 if ( i != j &&  isConnected[i][j]==1){
-                  adjlis[i].push_back( j);
-                     adjlis[j].push_back(i);
+        int m = isConnected[0].size();
+        vector<int> adjlist[n];
+        for ( int i =0;i < n ; i++ ){
+             for ( int j =0; j < m ;j++){
+                 if ( isConnected[i][j]== 1 && i != j){
+                     adjlist[i].push_back(j);
+                      adjlist[j].push_back(i);
                  }
              }
-         }
-        
+        }
         int count =0;
-        for ( int i =0 ; i < vis.size();i++){
-            if ( vis[i]!= 1){    
-                count ++;
-                vis[i]= 1;
-                 dfs ( i , vis ,adjlis);
+        vector <int > vis (n,0);
+//         queue <int > q ;
+//         q.push(0);
+//         vis[0]=1;
+        
+//         while( !q.empty()){
+//             int node = q.front();
+//             q.pop();
+          
+//             for ( auto it : adjlist[node]){
+//                 if ( !vis[it]){
+//                     vis[it]=1;
+//                     q.push(it);
+//                 }
+//             }
+             
+//         }
+        
+        for ( int i =0; i < vis.size(); i++){
+            if ( !vis[i]){
+                count++;
+            dfs (adjlist ,vis,count , i);
             }
         }
-        
-        
         return count ;
     }
+   
 };
