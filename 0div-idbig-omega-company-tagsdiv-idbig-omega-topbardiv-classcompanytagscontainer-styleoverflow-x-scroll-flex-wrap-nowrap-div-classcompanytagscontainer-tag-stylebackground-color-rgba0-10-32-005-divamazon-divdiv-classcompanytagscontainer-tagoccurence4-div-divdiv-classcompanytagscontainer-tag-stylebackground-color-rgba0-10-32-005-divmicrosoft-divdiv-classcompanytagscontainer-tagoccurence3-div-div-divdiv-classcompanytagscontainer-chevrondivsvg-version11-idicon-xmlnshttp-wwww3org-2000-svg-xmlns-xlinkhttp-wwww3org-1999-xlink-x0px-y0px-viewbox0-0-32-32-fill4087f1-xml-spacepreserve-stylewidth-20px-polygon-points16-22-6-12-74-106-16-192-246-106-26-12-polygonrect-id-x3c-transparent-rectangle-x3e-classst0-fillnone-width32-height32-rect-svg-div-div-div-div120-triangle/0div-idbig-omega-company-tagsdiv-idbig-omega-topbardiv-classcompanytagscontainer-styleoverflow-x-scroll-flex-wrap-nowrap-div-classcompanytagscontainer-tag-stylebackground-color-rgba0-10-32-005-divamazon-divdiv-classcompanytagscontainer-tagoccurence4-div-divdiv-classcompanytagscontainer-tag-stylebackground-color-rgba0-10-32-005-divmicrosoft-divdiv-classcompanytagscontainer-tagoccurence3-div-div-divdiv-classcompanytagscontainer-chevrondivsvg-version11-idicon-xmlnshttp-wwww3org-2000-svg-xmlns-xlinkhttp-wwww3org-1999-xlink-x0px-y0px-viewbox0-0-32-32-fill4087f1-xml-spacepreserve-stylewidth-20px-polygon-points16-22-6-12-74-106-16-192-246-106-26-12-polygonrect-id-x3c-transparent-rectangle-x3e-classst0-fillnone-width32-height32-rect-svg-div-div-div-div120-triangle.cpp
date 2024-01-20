02875f2c2d0a8ddcaab145a101f3cca8805f0a50
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2024-01-20 17:26:36 +0530

    Time: 4 ms (77.24%), Space: 11.3 MB (5.29%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div120-triangle/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div120-triangle.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div120-triangle/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div120-triangle.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& triangle, vector<vector<int>>& dp, int i, int j) {
-        if (i == triangle.size() - 1) {
-            return triangle[i][j];
-        }
+//     int solve(vector<vector<int>>& triangle, vector<vector<int>>& dp, int i, int j) {
+//         if (i == triangle.size() - 1) {
+//             return triangle[i][j];
+//         }
         
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
+//         if (dp[i][j] != -1) {
+//             return dp[i][j];
+//         }
 
-        int d = triangle[i][j] + solve(triangle, dp, i + 1, j);
-        int dg = triangle[i][j] + solve(triangle, dp, i + 1, j + 1);
-        dp[i][j] = min(d, dg);
+//         int d = triangle[i][j] + solve(triangle, dp, i + 1, j);
+//         int dg = triangle[i][j] + solve(triangle, dp, i + 1, j + 1);
+//         dp[i][j] = min(d, dg);
         
-        return dp[i][j];
+//         return dp[i][j];
+//     }
+
+  int minimumTotal(vector<vector<int>>& triangle) {
+    vector<vector<int>> dp(triangle.size(), vector<int>(triangle.size(), -1));
+
+    // Initialize the bottom row of dp
+    for (int i = 0; i < triangle.size(); i++) {
+        dp[triangle.size() - 1][i] = triangle[triangle.size() - 1][i];
     }
 
-    int minimumTotal(vector<vector<int>>& triangle) {
-        vector<vector<int>> dp(triangle.size(), vector<int>(triangle.size(), -1));  // Updated initialization
-        int i = 0;
-        int j = 0;
+    // Bottom-up dynamic programming
+    for (int i = triangle.size() - 2; i >= 0; i--) {
+        for (int j = 0; j <= i; j++) {  // Corrected loop condition
+            int d = triangle[i][j] + dp[i + 1][j];
+            int dg = triangle[i][j] + dp[i + 1][j + 1];
 
-        return solve(triangle, dp, i, j);
+            dp[i][j] = min(d, dg);
+        }
     }
+
+    return dp[0][0];
+}
 };
