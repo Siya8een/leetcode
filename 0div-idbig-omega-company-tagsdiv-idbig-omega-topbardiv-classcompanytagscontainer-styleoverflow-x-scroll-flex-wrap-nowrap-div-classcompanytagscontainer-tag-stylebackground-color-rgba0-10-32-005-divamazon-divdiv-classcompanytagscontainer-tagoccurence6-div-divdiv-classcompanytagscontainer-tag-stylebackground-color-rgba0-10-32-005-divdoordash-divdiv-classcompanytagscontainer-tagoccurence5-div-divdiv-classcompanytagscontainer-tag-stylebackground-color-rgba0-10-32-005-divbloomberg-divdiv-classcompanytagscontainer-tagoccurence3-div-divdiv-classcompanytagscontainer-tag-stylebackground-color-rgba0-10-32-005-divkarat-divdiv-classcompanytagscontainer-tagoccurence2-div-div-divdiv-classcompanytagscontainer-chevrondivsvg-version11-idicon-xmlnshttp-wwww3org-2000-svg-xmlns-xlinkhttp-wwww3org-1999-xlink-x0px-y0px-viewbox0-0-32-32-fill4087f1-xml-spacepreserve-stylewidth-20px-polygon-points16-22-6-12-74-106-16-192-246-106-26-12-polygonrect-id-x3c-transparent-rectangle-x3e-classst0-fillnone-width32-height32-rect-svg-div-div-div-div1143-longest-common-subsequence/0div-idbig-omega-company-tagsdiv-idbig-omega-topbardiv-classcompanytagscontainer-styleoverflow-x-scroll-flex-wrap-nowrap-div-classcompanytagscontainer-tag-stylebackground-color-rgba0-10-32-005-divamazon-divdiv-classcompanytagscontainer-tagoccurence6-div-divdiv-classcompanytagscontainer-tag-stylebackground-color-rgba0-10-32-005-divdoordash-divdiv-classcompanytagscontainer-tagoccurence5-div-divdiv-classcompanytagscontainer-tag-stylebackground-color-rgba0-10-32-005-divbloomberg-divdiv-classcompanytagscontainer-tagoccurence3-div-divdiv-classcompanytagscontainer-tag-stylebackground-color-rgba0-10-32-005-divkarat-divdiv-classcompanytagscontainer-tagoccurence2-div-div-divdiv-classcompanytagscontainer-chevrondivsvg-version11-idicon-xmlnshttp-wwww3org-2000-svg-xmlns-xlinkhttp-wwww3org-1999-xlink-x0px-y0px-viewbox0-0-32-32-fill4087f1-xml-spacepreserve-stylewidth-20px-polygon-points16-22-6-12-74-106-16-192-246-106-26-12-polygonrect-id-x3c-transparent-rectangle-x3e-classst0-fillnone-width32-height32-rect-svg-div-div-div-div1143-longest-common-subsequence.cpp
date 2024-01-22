a3502d5f916393555197e52d0c7f9a27f99d3519
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2024-01-22 21:25:32 +0530

    Time: 40 ms (72.73%), Space: 25.9 MB (5.05%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divdoordash-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divkarat-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1143-longest-common-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divdoordash-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divkarat-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1143-longest-common-subsequence.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int solve(int index1, int index2, const string& text1, const string& text2, vector<vector<int>>& dp) {
+        if (index1 < 0 || index2 < 0) {
+            return 0;
+        }
+
+        if (dp[index1][index2] != -1) {
+            return dp[index1][index2];
+        }
+
+        if (text1[index1] == text2[index2]) {
+            return dp[index1][index2] = 1 + solve(index1 - 1, index2 - 1, text1, text2, dp);
+        }
+
+        dp[index1][index2] = max(solve(index1 - 1, index2, text1, text2, dp), solve(index1, index2 - 1, text1, text2, dp));
+        return dp[index1][index2];
+    }
+
+    int longestCommonSubsequence(string text1, string text2) {
+        int index1 = text1.length();
+        int index2 = text2.length();
+        vector<vector<int>> dp(index1 + 1, vector<int>(index2 + 1, -1));
+        return solve(index1 - 1, index2 - 1, text1, text2, dp);
+    }
+};
