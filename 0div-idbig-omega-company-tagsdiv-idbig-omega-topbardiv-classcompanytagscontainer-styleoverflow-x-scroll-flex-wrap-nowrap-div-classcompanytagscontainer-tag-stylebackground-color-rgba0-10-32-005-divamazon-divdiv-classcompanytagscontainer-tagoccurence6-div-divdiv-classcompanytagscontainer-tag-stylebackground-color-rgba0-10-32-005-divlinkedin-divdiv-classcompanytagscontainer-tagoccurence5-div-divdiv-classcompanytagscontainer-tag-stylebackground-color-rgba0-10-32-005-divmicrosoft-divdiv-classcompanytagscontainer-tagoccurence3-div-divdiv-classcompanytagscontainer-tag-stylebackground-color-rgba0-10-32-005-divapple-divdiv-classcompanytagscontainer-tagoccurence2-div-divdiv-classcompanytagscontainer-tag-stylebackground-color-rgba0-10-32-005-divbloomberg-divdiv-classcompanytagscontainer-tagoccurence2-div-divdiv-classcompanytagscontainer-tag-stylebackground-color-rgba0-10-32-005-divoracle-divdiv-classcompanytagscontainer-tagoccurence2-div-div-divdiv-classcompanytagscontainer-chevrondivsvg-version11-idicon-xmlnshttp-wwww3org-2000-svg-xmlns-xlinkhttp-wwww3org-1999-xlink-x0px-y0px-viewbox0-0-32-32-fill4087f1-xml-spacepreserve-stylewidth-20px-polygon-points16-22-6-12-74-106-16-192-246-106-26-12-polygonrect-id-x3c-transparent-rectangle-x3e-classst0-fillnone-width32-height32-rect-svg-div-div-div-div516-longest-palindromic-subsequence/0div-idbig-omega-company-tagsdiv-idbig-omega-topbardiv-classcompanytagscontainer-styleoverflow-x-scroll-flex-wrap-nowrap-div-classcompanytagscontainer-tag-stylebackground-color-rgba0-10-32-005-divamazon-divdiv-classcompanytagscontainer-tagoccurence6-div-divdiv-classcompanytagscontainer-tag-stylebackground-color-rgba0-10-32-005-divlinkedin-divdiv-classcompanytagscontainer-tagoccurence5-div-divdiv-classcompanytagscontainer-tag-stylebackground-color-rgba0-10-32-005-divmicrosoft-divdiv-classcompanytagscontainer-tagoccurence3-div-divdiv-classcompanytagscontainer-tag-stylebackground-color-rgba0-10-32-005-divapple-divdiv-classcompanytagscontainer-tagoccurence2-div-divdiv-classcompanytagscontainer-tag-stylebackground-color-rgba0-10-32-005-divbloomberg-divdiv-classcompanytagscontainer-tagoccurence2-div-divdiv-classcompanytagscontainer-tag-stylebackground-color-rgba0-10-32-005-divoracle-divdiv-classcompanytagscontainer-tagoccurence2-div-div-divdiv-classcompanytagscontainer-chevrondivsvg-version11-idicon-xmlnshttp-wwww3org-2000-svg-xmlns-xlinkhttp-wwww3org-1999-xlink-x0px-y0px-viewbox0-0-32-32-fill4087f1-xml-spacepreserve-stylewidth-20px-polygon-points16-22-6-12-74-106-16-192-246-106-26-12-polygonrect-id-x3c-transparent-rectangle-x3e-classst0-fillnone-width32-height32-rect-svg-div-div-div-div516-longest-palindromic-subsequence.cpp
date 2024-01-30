130229a0205243a0b703e526c86076eb02bf0ad0
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2024-01-31 01:57:14 +0530

    Time: 75 ms (70.22%), Space: 74.4 MB (7.56%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div516-longest-palindromic-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div516-longest-palindromic-subsequence.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int solveCommonSubsequence(int index1, int index2, string &s1, string &s2, vector<vector<int>> &memo) {
+        if (index1 < 0 || index2 < 0) {
+            return 0;
+        }
+
+        if (memo[index1][index2] != -1) {
+            return memo[index1][index2];
+        }
+
+        int a = 0, b = 0;
+
+        if (s1[index1] == s2[index2]) {
+            a = 1 + solveCommonSubsequence(index1 - 1, index2 - 1, s1, s2, memo);
+        } else {
+            a = max(solveCommonSubsequence(index1, index2 - 1, s1, s2, memo),
+                    solveCommonSubsequence(index1 - 1, index2, s1, s2, memo));
+        }
+
+        memo[index1][index2] = a;
+        return a;
+    }
+
+    int longestPalindromeSubseq(string s) {
+        string s1 = s;
+        reverse(s.begin(), s.end());
+        string s2 = s;
+
+        int n = s1.length();
+        vector<vector<int>> memo(n, vector<int>(n, -1));
+
+        return solveCommonSubsequence(n - 1, n - 1, s1, s2, memo);
+    }
+};
