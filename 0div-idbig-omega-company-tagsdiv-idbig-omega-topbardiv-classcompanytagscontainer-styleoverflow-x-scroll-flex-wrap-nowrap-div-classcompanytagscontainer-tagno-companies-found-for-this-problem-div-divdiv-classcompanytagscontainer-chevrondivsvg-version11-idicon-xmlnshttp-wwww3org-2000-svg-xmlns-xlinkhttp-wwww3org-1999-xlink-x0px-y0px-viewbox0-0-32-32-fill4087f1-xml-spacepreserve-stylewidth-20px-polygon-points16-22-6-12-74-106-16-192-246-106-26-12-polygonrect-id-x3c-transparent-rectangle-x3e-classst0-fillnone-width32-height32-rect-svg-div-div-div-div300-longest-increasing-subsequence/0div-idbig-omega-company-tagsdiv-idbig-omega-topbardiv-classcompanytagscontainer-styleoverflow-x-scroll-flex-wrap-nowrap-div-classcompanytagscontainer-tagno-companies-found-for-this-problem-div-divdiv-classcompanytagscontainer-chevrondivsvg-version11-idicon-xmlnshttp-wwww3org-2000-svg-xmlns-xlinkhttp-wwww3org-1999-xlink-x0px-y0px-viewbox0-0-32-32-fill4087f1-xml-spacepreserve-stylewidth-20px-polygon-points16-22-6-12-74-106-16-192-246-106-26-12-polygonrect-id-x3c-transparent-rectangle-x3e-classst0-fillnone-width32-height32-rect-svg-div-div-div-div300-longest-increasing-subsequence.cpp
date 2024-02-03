f6e75f37d5426180c5cd1402b5a6c8e24c3883a7
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2024-02-03 18:51:39 +0530

    Time: 162 ms (61.92%), Space: 13.1 MB (23.59%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence.cpp
@@ -27,18 +27,21 @@ public:
 //     }
 
     int lengthOfLIS(std::vector<int>& nums) {
-        int n = nums.size();
-        vector < vector < int >> dp( n+1 , vector < int > ( n+1 , 0));
-        for ( int index =  n-1 ; index >=0 ;index--){
-            for ( int prev_ind  =index-1 ; prev_ind >= -1 ; prev_ind--){
-                int len = 0 +  dp [index+1][prev_ind + 1];
-                if ( prev_ind == -1 || nums[index ]> nums[prev_ind]){
-                    len = max ( len , 1+ dp[index+1][index+1]);
-                }
-                 dp [index ][prev_ind + 1]= len ;
+    int n = nums.size();
+    std::vector<int> next(n + 1, 0), cur(n + 1, 0);
+
+    for (int index = n - 1; index >= 0; index--) {
+        for (int prev_ind = index - 1; prev_ind >= -1; prev_ind--) {
+            int len = 0 + next[prev_ind + 1];
+            if (prev_ind == -1 || nums[index] > nums[prev_ind]) {
+                len = std::max(len, 1 + next[index + 1]);
             }
+            cur[prev_ind + 1] = len;
         }
-         return dp[0][0];
+        next = cur;
     }
+
+    return next[0];
+}
     
 };
