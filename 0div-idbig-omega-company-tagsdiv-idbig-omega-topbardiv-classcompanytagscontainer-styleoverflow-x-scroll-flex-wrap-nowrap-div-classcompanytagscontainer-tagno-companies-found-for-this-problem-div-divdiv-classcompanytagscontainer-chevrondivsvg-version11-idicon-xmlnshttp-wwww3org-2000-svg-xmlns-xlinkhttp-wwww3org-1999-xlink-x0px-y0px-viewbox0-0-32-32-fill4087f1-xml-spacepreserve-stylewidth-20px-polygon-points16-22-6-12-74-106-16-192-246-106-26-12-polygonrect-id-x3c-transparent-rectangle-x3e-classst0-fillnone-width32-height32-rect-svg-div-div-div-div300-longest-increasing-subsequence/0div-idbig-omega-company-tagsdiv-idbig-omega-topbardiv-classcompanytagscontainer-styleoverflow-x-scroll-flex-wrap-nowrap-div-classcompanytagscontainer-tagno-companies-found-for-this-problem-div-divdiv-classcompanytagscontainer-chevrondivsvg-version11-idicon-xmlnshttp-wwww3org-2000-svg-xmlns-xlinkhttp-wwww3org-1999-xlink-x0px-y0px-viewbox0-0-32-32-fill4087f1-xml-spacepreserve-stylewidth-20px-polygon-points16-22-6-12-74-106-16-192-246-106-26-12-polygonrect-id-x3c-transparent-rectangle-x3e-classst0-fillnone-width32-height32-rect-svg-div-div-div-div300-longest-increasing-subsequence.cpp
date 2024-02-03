3deeb3af65e62332d5cb707d2a9cf2d5ff7ccb3e
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2024-02-03 18:44:41 +0530

    Time: 234 ms (52.74%), Space: 294.6 MB (5.04%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence.cpp
@@ -3,34 +3,42 @@
 
 class Solution {
 public:
-    int solve(int index, int prev, std::vector<int>& nums, std::vector<std::vector<int>>& memo) {
-        if (index == nums.size() ) {
-            return 0;
-        }
+//     int solve(int index, int prev, std::vector<int>& nums, std::vector<std::vector<int>>& memo) {
+//         if (index == nums.size() ) {
+//             return 0;
+//         }
 
-        if (memo[index][prev+1] != -1) {
-            return memo[index][prev+1];
-        }
+//         if (memo[index][prev+1] != -1) {
+//             return memo[index][prev+1];
+//         }
 
-        int take = 0, notTake = 0;
+//         int take = 0, notTake = 0;
 
-        // Take the current element if it is greater than the previous element
-        if (prev == -1 || (index < nums.size() && nums[index] > nums[prev])) {
-            take = 1 + solve(index + 1, index, nums, memo);
-        }
+//         // Take the current element if it is greater than the previous element
+//         if (prev == -1 || (index < nums.size() && nums[index] > nums[prev])) {
+//             take = 1 + solve(index + 1, index, nums, memo);
+//         }
 
-        // Skip the current element
-        notTake = solve(index + 1, prev, nums, memo);
+//         // Skip the current element
+//         notTake = solve(index + 1, prev, nums, memo);
 
-        memo[index][prev+1] = std::max(take, notTake);
-        return memo[index][prev+1];
-    }
+//         memo[index][prev+1] = std::max(take, notTake);
+//         return memo[index][prev+1];
+//     }
 
     int lengthOfLIS(std::vector<int>& nums) {
         int n = nums.size();
-        // Initialize memoization table with -1
-        std::vector<std::vector<int>> memo(n+1 , std::vector<int>(n + 1, -1));
-
-        return solve(0 , -1, nums, memo);
+        vector < vector < int >> dp( n+1 , vector < int > ( n+1 , 0));
+        for ( int index =  n-1 ; index >=0 ;index--){
+            for ( int prev_ind  =index-1 ; prev_ind >= -1 ; prev_ind--){
+                int len = 0 +  dp [index+1][prev_ind + 1];
+                if ( prev_ind == -1 || nums[index ]> nums[prev_ind]){
+                    len = max ( len , 1+ dp[index+1][index+1]);
+                }
+                 dp [index ][prev_ind + 1]= len ;
+            }
+        }
+         return dp[0][0];
     }
+    
 };
