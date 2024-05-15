46896e73ebc5e14ec167ffa65aeadd97764c483a
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2024-05-15 11:43:53 +0530

    Time: 4 ms (58.49%), Space: 14.4 MB (99.44%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        // Find dip
+        int index = -1;
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            if (nums[i + 1] > nums[i]) {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) {
+            // If no dip found, reverse the entire sequence
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        
+        // Find just larger element to swap with
+        int justLarger = nums.size() - 1;
+        while (justLarger > index && nums[justLarger] <= nums[index]) {
+            justLarger--;
+        }
+        swap(nums[index], nums[justLarger]);
+        
+        // Reverse the remaining part to ensure smallest next permutation
+        reverse(nums.begin() + index + 1, nums.end());
     }
 };
