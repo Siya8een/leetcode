263a74aa69efb08ea99740dbb211068ca6e64fcc
Siya Rana <97952277+Siya8een@users.noreply.github.com>
2024-06-05 04:07:30 +0530

    Time: 4 ms (74.47%), Space: 12.4 MB (67.30%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,9 +1,8 @@
 #include <vector>
-using namespace std;
 
 class Solution {
 public:
-    void solve(vector<int>& a, vector<vector<int>>& ans, int i, vector<int>& candidates, int target) {
+    void solve(std::vector<int>& a, std::vector<std::vector<int>>& ans, int i, std::vector<int>& candidates, int target) {
         if (target == 0) {
             ans.push_back(a);
             return;
@@ -13,19 +12,20 @@ public:
         }
 
         // Take the current candidate
-        a.push_back(candidates[i]);
-        solve(a, ans, i, candidates, target - candidates[i]);
-        a.pop_back();
+        if (target >= candidates[i]) {
+            a.push_back(candidates[i]);
+            solve(a, ans, i, candidates, target - candidates[i]); // Not incrementing i allows multiple uses of the same element
+            a.pop_back();
+        }
 
         // Don't take the current candidate
         solve(a, ans, i + 1, candidates, target);
     }
 
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> a;
-        vector<vector<int>> ans;
-        int index = 0;
-        solve(a, ans, index, candidates, target);
+    std::vector<std::vector<int>> combinationSum(std::vector<int>& candidates, int target) {
+        std::vector<int> a;
+        std::vector<std::vector<int>> ans;
+        solve(a, ans, 0, candidates, target);
         return ans;
     }
 };
